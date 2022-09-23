b9b7866e474d9f132081bd40ab10bf6b16841101
Prathamesh Tanaji Savant <prathameshs2801@gmail.com>
2022-09-23 21:29:57 +0530

    file added

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'tic-tac-toe',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
